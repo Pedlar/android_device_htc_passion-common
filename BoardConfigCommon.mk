84d3007cb1440f9d27a30b2b367f6a2dd375982f
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2011-12-04 12:47:30 -0600

    steal some overlay and boardconfig items from crespo
    Change-Id: I602e7e8ecf9bd0ddf81e469140d4a486b9facd1b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -11,23 +11,27 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-# config.mk
 #
 # Product-specific compile-time definitions.
 #
 
+TARGET_BOOTLOADER_BOARD_NAME := mahimahi
+
 TARGET_NO_BOOTLOADER := true
 
-TARGET_BOARD_PLATFORM := qsd8k
+TARGET_BOARD_PLATFORM := qsd8k #QSD8250
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
 
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
+
+# Neon stuff
 TARGET_ARCH_VARIANT := armv7-a-neon
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
-TARGET_BOOTLOADER_BOARD_NAME := mahimahi
+# FPU compilation flags #Taken from crespo. Should I be using them?
+TARGET_GLOBAL_CFLAGS += -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp
 
 # Wifi related defines
 BOARD_WPA_SUPPLICANT_DRIVER := WEXT
@@ -42,20 +46,21 @@ WIFI_DRIVER_MODULE_NAME     := "bcm4329"
 
 BOARD_USES_GENERIC_AUDIO := false
 
+# Kernel
 BOARD_KERNEL_CMDLINE := no_console_suspend=1 wire.search_count=5
 BOARD_KERNEL_BASE := 0x20000000
 BOARD_KERNEL_NEW_PPPOX := true
 
 BOARD_USE_KINETO_COMPATIBILITY := true
-
 BOARD_VENDOR_QCOM_AMSS_VERSION := 3200
-
 BOARD_VENDOR_USE_AKMD := akm8973
 
 # Hardware rendering
+# doesn't actually work until we have hwcomposer
 USE_OPENGL_RENDERER := true
 BOARD_EGL_CFG := device/htc/passion-common/egl.cfg
-USE_OPENGL_RENDERER := true
+
+# Temp stuff for HWA hack
 BOARD_NO_RGBX_8888 := true
 BOARD_USES_OVERLAY := true
 COMMON_GLOBAL_CFLAGS += -DBOARD_GL_OES_EGL_IMG_EXTERNAL_HACK
@@ -65,39 +70,35 @@ BOARD_USES_QCOM_LIBS := true
 #TARGET_LIBAGL_USE_GRALLOC_COPYBITS := true
 #TARGET_USES_OLD_LIBSENSORS_HAL:=true
 
+# Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
 
-#BOARD_HAVE_FM_RADIO := true
-#BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
+# FM
+BOARD_HAVE_FM_RADIO := true
+BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
 
-# # cat /proc/mtd #AOSP
-# dev:    size   erasesize  name
-# mtd0: 000e0000 00020000 "misc"
-# mtd1: 00500000 00020000 "recovery"
-# mtd2: 00280000 00020000 "boot"
-# mtd3: 07800000 00020000 "system"
-# mtd4: 07800000 00020000 "cache"
-# mtd5: 0c440000 00020000 "userdata"
+# GPS HAL
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := mahimahi
+
+# AMSS version to use for GPS
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 3200
+
+# # cat /proc/mtd #AOSP                 # # cat /proc/mtd #CM7
+# dev:    size   erasesize  name        # dev:    size   erasesize  name
+# mtd0: 000e0000 00020000 "misc"        # mtd0: 000e0000 00020000 "misc"
+# mtd1: 00500000 00020000 "recovery"    # mtd1: 00400000 00020000 "recovery"
+# mtd2: 00280000 00020000 "boot"        # mtd2: 00380000 00020000 "boot"
+# mtd3: 07800000 00020000 "system"      # mtd3: 09100000 00020000 "system"
+# mtd4: 07800000 00020000 "cache"       # mtd4: 05f00000 00020000 "cache"
+# mtd5: 0c440000 00020000 "userdata"    # mtd5: 0c440000 00020000 "userdata"
 # mtd6: 00200000 00020000 "crashdata"
-# # cat /proc/mtd #CM
-#dev:    size   erasesize  name
-#mtd0: 000e0000 00020000 "misc"
-#mtd1: 00400000 00020000 "recovery"
-#mtd2: 00380000 00020000 "boot"
-#mtd3: 09100000 00020000 "system"
-#mtd4: 05f00000 00020000 "cache"
-#mtd5: 0c440000 00020000 "userdata"
 
+#TARGET_USERIMAGES_USE_EXT4 := true #not sure about this yet
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00300000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00400000
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 230686720 #0x09000000 #0x08400000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 209715200 #0x0c440000
-BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_FLASH_BLOCK_SIZE := 131072 #Is this too high? crespo uses 4096
 
 TARGET_RELEASETOOLS_EXTENSIONS := device/htc/common
-
-# to enable the GPS HAL
-BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := mahimahi
-# AMSS version to use for GPS
-BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 3200
